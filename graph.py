6290fede8c09226556c30446f55213aac8b28c5d
Thomas LINTANF <thomas.lintanf@efrei.fr>
2019-11-14 16:33:32 +0100

    Updated Class Graph to v2.0:  - Updated constructor to v2.0  - Added method detecttion circuit v1.0

--- graph.py
+++ graph.py
@@ -1,7 +1,7 @@
 """
     file : graph.py
     author(s) : Thomas LINTANF
-    Version : 1.0
+    Version : 2.0
 
     Definition de la classe Graph qui permet de stocker un graph orienter et de lui
     appliquer différent algorithme.
@@ -13,20 +13,21 @@ class Graph :
     """ 
         classe Graph  : représente un graphe orienté 
         
-        Version: 1.0
+        Version: 2.0
     """
 
     def __init__(self):
         """ 
             constructeur de la classe Graph
 
-            Version: 1.0
+            Version: 2.0
         """
 
         self.nbSommets = 0
         self.nbArcs = 0
         self.mAdjacence = []
         self.mValeurs = []
+        self.contientCircuit = 0
 
     def readFile(self,address):
         """
@@ -69,7 +70,7 @@ class Graph :
         """
             fonction de représentation au format string
 
-            Version 1.0
+            Version: 1.0
         """
 
         s = "Graphe :\n - {0} sommets`\n - {1} arcs\nMatrice d'Adjacence :\n".format(self.nbSommets,self.nbArcs)
@@ -79,3 +80,40 @@ class Graph :
         for ligne in self.mValeurs:
             s+= str(ligne) + '\n'
         return s
+
+    def detectionCircuit(self):
+        """
+            Cherche si le graphe contient un circuit
+            Retourne True si le graphe contient au moins un circuit False sinon
+            ecrit également le resultat sur la propriété contient circuit
+
+            Version: 1.0
+        """
+
+        lSommets = list(range(0,self.nbSommets))
+
+        continuer = True
+        while(continuer):
+            continuer = False
+            sToDel = []
+
+            # Recherche des sommets sans prédécesseur
+            for sommet in lSommets:
+                pred = False
+                for s in lSommets:
+                    pred = pred | self.mAdjacence[s][sommet]
+                
+                if(pred == False):
+                    sToDel.append(sommet)
+            
+            # Suppression des sommets sans prédécesseur
+            for s in sToDel:
+                lSommets.remove(s)
+
+            # Sortie de boucle si on a pas retirer de sommets ou qu'il n'en reste plus
+            continuer = len(sToDel)>0 & len(lSommets)>0
+
+        # On regarde si il reste des Sommets pour savoir si il y a un circuit
+        self.contientCircuit = len(lSommets) != 0
+
+        return self.contientCircuit
